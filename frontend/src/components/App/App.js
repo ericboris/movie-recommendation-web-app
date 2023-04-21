bd27d5d44f5909eda969497e58854240a036590b
icosceles <icosceles@tutanota.com>
2023-04-21 14:44:14 -0700

    add UserProfile component

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import LoginForm from '../LoginForm/LoginForm';
 import SignupForm from '../SignupForm/SignupForm';
+import UserProfile from '../UserProfile/UserProfile';
 
 function App() {
     const handleLogin = () => {
@@ -12,12 +13,15 @@ function App() {
         console.log("Signed up!"); 
     };
 
+    const userProfile = { id:1, email:"UserEmail@Email.com" };
+
     return (
         <div className="App">
             <h1>Movie Recommendation App</h1>
             <nav>
                 <LoginForm onLogin={handleLogin} />
                 <SignupForm onSignup={handleSignup} />
+                <UserProfile user={userProfile} />
             </nav>
         </div>
     );
--- frontend/src/components/UserProfile/UserProfile.js
+++ frontend/src/components/UserProfile/UserProfile.js
@@ -1,4 +1,69 @@
-// Fetch user data and display it (e.g. call API, render user data)
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+// import UserService from './services/UserService';
+import './UserProfile.css';
 
-// Fetch user's rated movies and display them (e.g. call API, render movie list)
+const UserProfile = ({ user }) => {
+    const [profile, setProfile] = useState(null);
+    const [ratedMovies, setRatedMovies] = useState([]);
+    
+    useEffect(() => {
+        async function fetchProfile() {
+            // const userProfile = await UserService.getUserProfile(user.id);
+            const userProfile = { 
+                name: "BillyBob NameName", 
+                ratedMovies: [
+                    { id:1, title:'Movie1', rating:1 }, 
+                    { id:2, title:'Movie2', rating:2 }, 
+                    { id:3, title:'Movie3', rating:3 }, 
+                ]
+            };
+            setProfile(userProfile);
+            setRatedMovies(userProfile.ratedMovies);
+        }
+        fetchProfile();
+    }, [user.id]);
 
+    const handleProfileUpdate = async (updatedProfile) => {
+        // const newProfile = await UserService.updateUserProfile(user.id, updatedProfile);
+        console.log('Profile Update');
+        setProfile(newProfile);
+    };
+
+    const handleDeleteProfile = async () => {
+        // await UserService.deleteUserProfile(user.id);
+        console.log('Profile Delete');
+        // Handle user logout or redirection after profile deletion
+    };
+
+    if (!profile) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div className="user-profile">
+            <h2>User Profile</h2>
+            <p>Email: {user.email}</p>
+            <p>Name: {profile.name}</p>
+            <h3>Rated Movies</h3>
+            <ul>
+                {ratedMovies.map((movie) => (
+                    <li key={movie.id}>
+                        {movie.title} - {movie.rating} stars
+                    </li>
+                ))}
+            </ul>
+            {/* <button onClick={() => handleProfileUpdate(updatedProfile)}>Update Profile</button> */}
+            {/* <button onClick={handleDeleteProfile}>Delete Profile</button> */}
+        </div>
+    );
+};
+
+UserProfile.propTypes = {
+    user: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        email: PropTypes.string.isRequired,
+    }).isRequired,
+};
+
+export default UserProfile;
