ac9ead3d51fdcef650c1ad924ec19eb1d3b6c710
icosceles <icosceles@tutanota.com>
2023-04-26 16:19:39 -0700

    update the movie rating model and remove the others

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,3 +1,1 @@
-from .user import User
 from .movie_rating import MovieRating
-from .movie_review import MovieReview
--- backend/app/models/movie_rating.py
+++ backend/app/models/movie_rating.py
@@ -4,12 +4,14 @@ from ..database import db
 class MovieRating(db.Model):
     __tablename__ = 'movie_ratings'
 
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    movie_id = db.Column(db.String, nullable=False)
-    rating = db.Column(db.Float, nullable=False)
-    timestamp = db.Column(db.DateTime, nullable=False)
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    wallet_address = db.Column(db.String(42), nullable=False)
+    movie_id = db.Column(db.Integer, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.TIMESTAMP, server_default=db.func.current_timestamp(), nullable=False)
+    updated_at = db.Column(db.TIMESTAMP, server_default=db.func.current_timestamp(), onupdate=db.func.current_timestamp(), nullable=False)
 
     __table_args__ = (
-        CheckConstraint('rating >= 0 AND rating <= 5', name='rating_range'),
+        db.UniqueConstraint('wallet_address', 'movie_id', name='uq_wallet_movie'),
+        CheckConstraint('rating >= 0 AND rating <= 5', name='rating_range')
     )
