ea29fab247f00c7e2876f3c63491a9fac3a15c9e
icosceles <icosceles@tutanota.com>
2023-04-28 12:07:56 -0700

    add movie posters and details to staticMovieList

--- frontend/src/staticMovieList.js
+++ frontend/src/staticMovieList.js
@@ -1,40 +1,93 @@
-const movieList = [
+const movies = [
     {
         id: 1,
         title: "The Shawshank Redemption",
-        releaseDate: "1994-09-22",
-        description: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency."
+        poster: "https://duckduckgo.com/i/905a37a3.jpg",
+        releaseYear: "1994",
+        director: "Frank Darabont",
+        description: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+        actors: ["Tim Robbins", "Morgan Freeman", "Bob Gunton"]
     },
     {
         id: 2,
         title: "The Godfather",
-        releaseDate: "1972-03-15",
-        description: "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son."
+        poster: "https://duckduckgo.com/i/f4222511.jpg",
+        releaseYear: "1972",
+        director: "Francis Ford Coppola",
+        description: "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+        actors: ["Marlon Brando", "Al Pacino", "James Caan"]
     },
     {
         id: 3,
-        title: "Pulp Fiction",
-        releaseDate: "1994-09-10",
-        description: "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption."
+        title: "The Dark Knight",
+        poster: "https://duckduckgo.com/i/9658742f.jpg",
+        releaseYear: "2008",
+        director: "Christopher Nolan",
+        description: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+        actors: ["Christian Bale", "Heath Ledger", "Aaron Eckhart"]
     },
     {
         id: 4,
-        title: "The Dark Knight",
-        releaseDate: "2008-07-18",
-        description: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+        title: "Forrest Gump",
+        poster: "https://duckduckgo.com/i/d9ffa182.jpg",
+        releaseYear: "1994",
+        director: "Robert Zemeckis",
+        description: "The presidencies of Kennedy and Johnson, the events of Vietnam, Watergate and other historical events unfold through the perspective of an Alabama man with an IQ of 75, whose only desire is to be reunited with his childhood sweetheart.",
+        actors: ["Tom Hanks", "Robin Wright", "Gary Sinise"]
     },
     {
         id: 5,
-        title: "Forrest Gump",
-        releaseDate: "1994-07-06",
-        description: "The presidencies of Kennedy and Johnson, the events of Vietnam, Watergate and other historical events unfold through the perspective of an Alabama man with an IQ of 75, whose only desire is to be reunited with his childhood sweetheart.",
+        title: "The Matrix",
+        poster: "https://duckduckgo.com/i/afe1ef49.jpg",
+        releaseYear: "1999",
+        director: "Lana Wachowski, Lilly Wachowski",
+        description: "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
+        actors: ["Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss"]
     },
     {
         id: 6,
-        title: "The Matrix",
-        releaseDate: "1999-03-31",
-        description: "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
+        title: "Schindler's List",
+        poster: "https://duckduckgo.com/i/f05b2f43.jpg",
+        releaseYear: "1993",
+        director: "Steven Spielberg",
+        description: "In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.",
+        actors: ["Liam Neeson", "Ben Kingsley", "Ralph Fiennes"]
     },
+    {
+        id: 7,
+        title: "Pulp Fiction",
+        poster: "https://duckduckgo.com/i/2534d02a.jpg",
+        releaseYear: "1994",
+        director: "Quentin Tarantino",
+        description: "The lives of two mob hit men, a boxer, a gangster's wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+        actors: ["John Travolta", "Samuel L. Jackson", "Uma Thurman"]
+    },
+    {
+        id: 8,
+        title: "The Silence of the Lambs",
+        poster: "https://duckduckgo.com/i/7b551875.jpg",
+        releaseYear: "1991",
+        director: "Jonathan Demme",
+        description: "A young FBI cadet must confide in an incarcerated and manipulative killer to receive his help on catching another serial killer who skins his victims.",
+        actors: ["Jodie Foster", "Anthony Hopkins", "Lawrence A. Bonney"]
+    },
+    {
+        id: 9,
+        title: "The Lord of the Rings: The Fellowship of the Ring",
+        poster: "https://duckduckgo.com/i/6f66a21e.jpg",
+        releaseYear: "2001",
+        director: "Peter Jackson",
+        description: "A meek hobbit of the Shire and eight companions set out on a journey to Mount Doom to destroy the One Ring and the dark lord Sauron.",
+        actors: ["Elijah Wood", "Ian McKellen", "Orlando Bloom"]
+    },
+    {
+        id: 10,
+        title: "Inception",
+        poster: "https://duckduckgo.com/i/a7e7fa37.jpg",
+        releaseYear: "2010",
+        director: "Christopher Nolan",
+        description: "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
+        actors: ["Leonardo DiCaprio", "Joseph Gordon-Levitt", "Ellen Page"]
+    }
 ];
-
-export default movieList;
+export default movies;
