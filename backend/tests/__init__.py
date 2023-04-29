105c34b7c0c2061f3f1fd8339c912637e109fa99
icosceles <icosceles@tutanota.com>
2023-04-29 12:16:42 -0700

    add basic testing

---
+++ backend/tests/__init__.py
@@ -0,0 +1,2 @@
+import warnings
+warnings.filterwarnings("ignore", message="LightFM was compiled without OpenMP support")
---
+++ backend/tests/test_views.py
@@ -0,0 +1,16 @@
+import pytest
+from flask_testing import TestCase
+from app import create_app
+
+class TestViews(TestCase):
+
+    def create_app(self):
+        app = create_app()
+        app.config['TESTING'] = True
+        return app
+
+    def test_example_route(self):
+        response = self.client.get('api/')
+
+        self.assert200(response)
+
